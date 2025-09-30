c82d7c6402c80ebbdbc030976aaff83c5e63e4b6
Harsh Tripathi <tripathi.ha@sms-datatech.co.jp>
2025-09-30 13:50:33 +0900

    (bugfix): Image config resolved

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',                // or 'https' if you switch later
+        hostname: 'genkimart.neovara.uk',
+        port: '',                        // no port
+        pathname: '/media/**',           // paths you allow
+      },
+    ],
+  },
 };
+
+
 
 export default nextConfig;
